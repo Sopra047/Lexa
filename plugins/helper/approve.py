84f5cc106f7eb07973d92af613f466ac8b4bc53c
KPEDJO <108293740+SUPEREL@users.noreply.github.com>
2024-01-25 18:40:12 +0100

    Update approve.py

--- plugins/helper/approve.py
+++ plugins/helper/approve.py
@@ -9,7 +9,7 @@ from info import CHAT_ID, TEXT, APPROVED
 async def autoapprove(client, message: ChatJoinRequest):
     chat=message.chat 
     user=message.from_user 
-    print(f"{user.first_name} Joined (Approved)") 
+    print(f"{user.first_name} 𝖣𝖾𝗆𝖺𝗇𝖽𝖾 (𝖠𝗉𝗉𝗋𝗈𝗎𝗏𝖾́)") 
     await client.approve_chat_join_request(chat_id=chat.id, user_id=user.id)
     if APPROVED == "on":
         await client.send_message(chat_id=chat.id, text=TEXT.format(mention=user.mention, title=chat.title))
