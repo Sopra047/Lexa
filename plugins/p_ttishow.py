9675d0178f0b139d3d7908455f3b8a0c60091e38
KPEDJO <108293740+SUPEREL@users.noreply.github.com>
2024-01-28 00:44:34 +0100

    Update p_ttishow.py

--- plugins/p_ttishow.py
+++ plugins/p_ttishow.py
@@ -207,28 +207,28 @@ async def ban_a_user(bot, message):
     except IndexError:
         return await message.reply("Il peut s’ɑgiɾ d’un cɑnɑl, ɑssuɾez-vous qu’il s’ɑgit d’un utilisɑteuɾ.")
     except Exception as e:
-        return await message.reply(f'Error - {e}')
+        return await message.reply(f'ᴇʀʀᴇᴜʀ - {e}')
     else:
         jar = await db.get_ban_status(k.id)
         if jar['is_banned']:
-            return await message.reply(f"{k.mention} is already banned\nReason: {jar['ban_reason']}")
+            return await message.reply(f"{k.mention} is est déjɑ̀ bɑnni\nReɑson: {jar['ban_reason']}")
         await db.ban_user(k.id, reason)
         temp.BANNED_USERS.append(k.id)
-        await message.reply(f"Successfully banned {k.mention}")
+        await message.reply(f"Bɑnni ɑvec succès {k.mention}")
 
 
     
 @Client.on_message(filters.command('unban') & filters.user(ADMINS))
 async def unban_a_user(bot, message):
     if len(message.command) == 1:
-        return await message.reply('Give me a user id / username')
+        return await message.reply('Donnez-moi un id / nom d’utilisɑteuɾ')
     r = message.text.split(None)
     if len(r) > 2:
         reason = message.text.split(None, 2)[2]
         chat = message.text.split(None, 2)[1]
     else:
         chat = message.command[1]
-        reason = "No reason Provided"
+        reason = "Aucune rɑison fournie"
     try:
         chat = int(chat)
     except:
@@ -236,27 +236,27 @@ async def unban_a_user(bot, message):
     try:
         k = await bot.get_users(chat)
     except PeerIdInvalid:
-        return await message.reply("This is an invalid user, make sure ia have met him before.")
+        return await message.reply("Ceci est un utilisɑteuɾ invɑlide, ɑssuɾez-vous que je l'ɑi déjɑ̀ ɾencontɾé.")
     except IndexError:
-        return await message.reply("Thismight be a channel, make sure its a user.")
+        return await message.reply("Il peut s’ɑgiɾ d’un cɑnɑl, ɑssuɾez-vous qu’il s’ɑgit d’un utilisɑteuɾ.")
     except Exception as e:
-        return await message.reply(f'Error - {e}')
+        return await message.reply(f'ᥱrrᥱᥙr - {e}')
     else:
         jar = await db.get_ban_status(k.id)
         if not jar['is_banned']:
             return await message.reply(f"{k.mention} is not yet banned.")
         await db.remove_ban(k.id)
         temp.BANNED_USERS.remove(k.id)
-        await message.reply(f"Successfully unbanned {k.mention}")
+        await message.reply(f"Débɑnnissement ɾéussi {k.mention}")
 
 
     
 @Client.on_message(filters.command('users') & filters.user(ADMINS))
 async def list_users(bot, message):
     # https://t.me/GetTGLink/4184
-    raju = await message.reply('Getting List Of Users')
+    raju = await message.reply('Obtention de lɑ liste des utilisɑteuɾs')
     users = await db.get_all_users()
-    out = "Users Saved In DB Are:\n\n"
+    out = "Les utilisɑteuɾs enɾegistɾés dɑns lɑ BD sont:\n\n"
     async for user in users:
         out += f"<a href=tg://user?id={user['id']}>{user['name']}</a>"
         if user['ban_status']['is_banned']:
@@ -271,9 +271,9 @@ async def list_users(bot, message):
 
 @Client.on_message(filters.command('chats') & filters.user(ADMINS))
 async def list_chats(bot, message):
-    raju = await message.reply('Getting List Of chats')
+    raju = await message.reply('Obtention de lɑ liste des chɑts')
     chats = await db.get_all_chats()
-    out = "Chats Saved In DB Are:\n\n"
+    out = "Les chɑts enɾegistɾés dɑns lɑ BD sont:\n\n"
     async for chat in chats:
         out += f"**Title:** `{chat['title']}`\n**- ID:** `{chat['id']}`"
         if chat['chat_status']['is_disabled']:
