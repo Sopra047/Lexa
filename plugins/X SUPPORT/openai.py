34ee670a744d2906a498d146e9039b7e1a547d79
NARUTO <150582863+Naruto46i@users.noreply.github.com>
2024-01-28 13:44:09 +0530

    Create openai.py

---
+++ plugins/X SUPPORT/openai.py
@@ -0,0 +1,41 @@
+from pyrogram import Client, filters
+from info import OPENAI_API, LOG_CHANNEL, AI
+import openai
+import asyncio
+
+openai.api_key = OPENAI_API
+
+async def send_message_in_chunks(client, chat_id, text):
+    max_length = 4096  # Maximum length of a message
+    for i in range(0, len(text), max_length):
+        await client.send_message(chat_id, text[i:i+max_length])
+
+
+@Client.on_message(filters.command("ask"))
+async def ask_question(client, message):
+    if AI == True: 
+        user_id = message.from_user.id
+        if user_id:
+            try:
+                msg = await message.reply_text("**ᴘʟᴇᴀsᴇ ᴡᴀɪᴛ ᴀ ᴍᴏᴍᴇɴᴛ ᴡʜɪʟᴇ ᴛʜᴇ ᴄʜᴀᴛʙᴏᴛ ʀᴇsᴘᴏɴᴅs ᴛᴏ ʏᴏᴜʀ ǫᴜᴇʀʏ . . .**")
+                text = message.text.split(" ", 1)[1]
+                user_id = message.from_user.id
+                response = openai.ChatCompletion.create(
+                    model="gpt-3.5-turbo",
+                    messages=[
+                        {"role": "system", "content": "You are a helpful assistant."},
+                        {"role": "user", "content": text}
+                    ],
+                    max_tokens=1200,  # Increase the value of max_tokens to allow for longer responses
+                    temperature=0.6
+                )          
+                ai_response = response.choices[0].message.content.strip()
+                await msg.delete()
+                await send_message_in_chunks(client, message.chat.id, f"**ʜᴇʀᴇ ɪs ʏᴏᴜʀ ᴀɴsᴡᴇʀ ʀᴇʟᴀᴛᴇᴅ ᴛᴏ ʏᴏᴜʀ ǫᴜᴇʀʏ** 👇\n\n{response.choices[0].message.content}")
+                await send_message_in_chunks(client, LOG_CHANNEL, f"**⭕ ᴀ ᴜsᴇʀ ɴᴀᴍᴇᴅ:** {message.from_user.mention} **ᴡɪᴛʜ ᴜsᴇʀ ɪᴅ -** {user_id}.\n🔍 **ᴀsᴋᴇᴅ ᴍᴇ ᴛʜɪs ǫᴜᴇʀʏ...**👇\n\n🔻 **ǫᴜᴇʀʏ:** `{text}`\n\n🔻 **ʜᴇʀᴇ ɪs ᴀɴsᴡᴇʀ ɪ ʀᴇsᴘᴏɴᴅᴇᴅ:**\n🖍️ {ai_response}\n\n\n🔻 **ᴜsᴇʀ ɪᴅ :-** {user_id} \n🔻 **ᴜsᴇʀ ɴᴀᴍᴇ :-** {message.from_user.mention}")
+                
+            except Exception as error:
+                print(error)
+                await message.reply_text(f"**An error occurred:**\n\n**{error}**\n\n")
+    else:
+        return
