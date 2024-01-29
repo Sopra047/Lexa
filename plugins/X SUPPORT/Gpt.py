8ad5e4f9994d964b75b560e5bab15a26f24fdb69
NARUTO <150582863+Naruto46i@users.noreply.github.com>
2024-01-29 08:29:24 +0530

    Update Gpt.py

--- plugins/X SUPPORT/Gpt.py
+++ plugins/X SUPPORT/Gpt.py
@@ -43,4 +43,4 @@ async def ai_res(message ,query):
                     ),
                     disable_web_page_preview=True,
                 )
-                await client.send_message(LOG_CHANNEL, text=f"#ask {userMention}\n\nAsked to Ai : {query}\n\nAi Res: {api_response}",          
+                await client.send_message(AI_LOGS, text=f"#ask Rᴇϙᴜᴇ̂ᴛᴇ ᴅᴇ {message.from_user.mention}")
