19c813b72232598fa12eb63d855a25ad17d5a817
KPEDJO <108293740+SUPEREL@users.noreply.github.com>
2024-01-31 09:37:16 +0100

    Update Restart.py

--- plugins/X SUPPORT/Restart.py
+++ plugins/X SUPPORT/Restart.py
@@ -8,5 +8,5 @@ from info import ADMINS
 async def stop_button(bot, message):
     msg = await bot.send_message(text="**🔄 Lᴇs ᴘʀᴏᴄᴇssᴜs sᴇ sᴏɴᴛ ᴀʀʀᴇ̂ᴛᴇ́s. Lᴇ ʀᴏʙᴏᴛ ʀᴇᴅᴇ́ᴍᴀʀʀᴇ...**", chat_id=message.chat.id)       
     await asyncio.sleep(3)
-    await msg.edit("**✅️ ʟᴇ ʙᴏᴛ ᴇsᴛ ʀᴇᴅᴇ́ᴍᴀʀʀᴇ́. ᴍᴀɪɴᴛᴇɴᴀɴᴛ﹐ ᴠᴏᴜs ᴘᴏᴜᴠᴇᴢ ᴍ’ᴜᴛɪʟɪsᴇʀ**")
+    await msg.edit("**✅️ ʟᴇ ʙᴏᴛ ᴇsᴛ ʀᴇᴅᴇ́ᴍᴀʀʀᴇ́. ᴍᴀɪɴᴛᴇɴᴀɴᴛ, ᴠᴏᴜs ᴘᴏᴜᴠᴇᴢ ᴍ’ᴜᴛɪʟɪsᴇʀ**")
     os.execl(sys.executable, sys.executable, *sys.argv)
