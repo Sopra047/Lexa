2540926a3da65a48be331692ae03a16888979600
Sopra <111066382+Sopra047@users.noreply.github.com>
2024-08-14 15:23:29 +0200

    Update carbon.py

--- plugins/helper/carbon.py
+++ plugins/helper/carbon.py
@@ -43,7 +43,7 @@ async def carbon_func(_, message):
     await m.edit("Téléchɑrgement..")
     await message.reply_photo(
         photo=carbon,
-        caption="**Cᥱttᥱ ρhoto ᥱst sყmρᥲ\nRᥱ́ᥲᥣιsᥱ́ ρᥲr @SHARCLUB**",
+        caption="**Cᥱttᥱ ρhoto ᥱst sყmρᥲ\nRᥱ́ᥲᥣιsᥱ́ ρᥲr @showgroups2**",
         reply_markup=InlineKeyboardMarkup([[InlineKeyboardButton("sᴜᴘᴘᴏʀᴛ", url=S_GROUP)]]),                   
     )
     await m.delete()
